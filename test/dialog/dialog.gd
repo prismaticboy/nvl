4f0884f3a020230ad2f0417daecfbf362e8308f5
prismaticboy <2779435956@qq.com>
2025-02-22 22:11:51 +0800

    creat new project

---
+++ test/dialog/dialog.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name dialog
+
+@export var character_name :String
+@export var content :String
+@export var avatar :Texture
+@export var location :Vector2
+@export var animation :String
+ 
---
+++ test/dialog/dialog_group.gd
@@ -0,0 +1,7 @@
+extends Resource
+
+
+class_name dialog_group
+
+
+@export var dialog_list:Array[dialog]
