0207f13d3bccd273ef9dc3089042bf6eeaac89ec
prismaticboy <2779435956@qq.com>
2025-02-24 00:07:39 +0800

    dialog test

--- test/dialog/diag_ui.gd
+++ test/dialog/diag_ui.gd
@@ -8,13 +8,32 @@ extends Control
 @export var dialog_day1:dialog_group
 
 var dialog_index=0
+var type_time = 0.025
+var pressed=false
 
 func diaplay_next_dialog():
+
+	if dialog_index>=len(dialog_day1.dialog_list):
+		visible = false
+		return
 	var dialog_1:=dialog_day1.dialog_list[dialog_index]
+	
 	char_name.text=dialog_1.character_name
 	char_content.text=dialog_1.content
-	
+	if $Panel/AnimationPlayer.is_playing():
+		$Panel/AnimationPlayer.play("RESET")
+		$Panel/AnimationPlayer.stop()
+	else:
+		$Panel/AnimationPlayer.playback_default_blend_time=(type_time*dialog_1.content.length())
+		$Panel/AnimationPlayer.play("show")
 	dialog_index+=1
+	
+	
+	
+	
+	
+	
+	#dialog_index+=1
 	
 func _ready() -> void:
 	diaplay_next_dialog()
@@ -23,4 +42,4 @@ func _ready() -> void:
 	
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index==MOUSE_BUTTON_LEFT and event.pressed:
-		diaplay_next_dialog()
+			diaplay_next_dialog()
--- test/dialog/dialog.gd
+++ test/dialog/dialog.gd
@@ -5,5 +5,5 @@ class_name dialog
 @export var character_name :String
 @export var content :String
 @export var avatar :Texture
-@export var animation :String
- 
+@export var animation :Resource
+@export var show:Array 
