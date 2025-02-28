5fd8f14fd238821b76904d2b70bdbb7ce2950c7b
prismaticboy <2779435956@qq.com>
2025-02-28 22:54:00 +0800

    add animation

--- test/dialog/diag_ui.gd
+++ test/dialog/diag_ui.gd
@@ -18,17 +18,12 @@ var type_time = 0.025
 var type_tween:Tween
 
 func diaplay_next_dialog():
-
+	
 	if dialog_index>=len(dialog_day1.dialog_list):
 		visible = false
 		return
 	var dialog_1:=dialog_day1.dialog_list[dialog_index]
-	for hide in dialog_1.hide:
-		if has_node(str(hide)):
-			get_node(str(hide)).queue_free()
-	for show in dialog_1.show:
-		var show_char=load("res://test/dialog/"+str(show)+".tscn")
-		add_child(show_char.instantiate())
+
 	char_content.visible_ratio=0
 	char_name.text=dialog_1.character_name
 	char_content.text=dialog_1.content
@@ -42,13 +37,21 @@ func diaplay_next_dialog():
 		type_tween = get_tree().create_tween()
 		type_tween.tween_property(char_content,"visible_ratio",1,type_time*char_content.text.length())
 		type_tween.tween_callback(func():dialog_index+=1)
-	
+		
+		for hide_name in dialog_1.hide:
+			if has_node(str(hide_name)):
+				get_node(str(hide_name)).queue_free()
+					
+		for show_name in dialog_1.show:
+			if show_name !=null:
+				var show_char=load("res://test/dialog/"+str(show_name)+".tscn").instantiate()
+				show_char.set_name(show_name)
+				add_child(show_char)
+	#if dialog_1.animation!=null and dialog_1.animation!="":	
+		get_node(dialog_1.character_id+"/AnimationPlayer").play(dialog_1.animation)	
+		
 func _ready() -> void:
 	diaplay_next_dialog()
-	#var char1=load("res://test/dialog/char1.tscn")
-	#var char2=load("res://test/dialog/char2.tscn")
-	#add_child(char1.instantiate())
-	#add_child(char2.instantiate())
 
 	
 func _input(event: InputEvent) -> void:
--- test/dialog/dialog.gd
+++ test/dialog/dialog.gd
@@ -4,8 +4,9 @@ class_name dialog
 
 
 @export var character_name :String
+@export var character_id :String
 @export var hide :Array
 @export var show :Array
 @export var content :String
 @export var pos:Vector2 
-@export var animation :String
+@export var animation:String
