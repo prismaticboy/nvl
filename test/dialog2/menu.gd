5a1859251427f147a53414a0580b7a14e0db141c
prismaticboy <2779435956@qq.com>
2025-04-27 21:54:37 +0800

    button emit signal

--- test/dialog2/menu.gd
+++ test/dialog2/menu.gd
@@ -4,6 +4,7 @@ signal save
 signal load
 var menu_bt_x:int=900
 var menu_bt_y:int=0
+var flag=false
 
 @onready var menu_bt: Label = $menu_bt
 @onready var save_bt: Label = $save_bt
@@ -12,22 +13,23 @@ var menu_bt_y:int=0
 
 
 func _ready() -> void:
-
-	
+	menu_bt.connect("gui_input",_menu_input)
+	exit_bt.connect("gui_input",_exit_input)
+	#menu_bt.gui_input.connect(_menu_input)
 	for bt in get_children():
 		if bt is Label:
 			bt.z_index=2
-			bt.size=Vector2(200,100)
+			bt.size=Vector2(100,50)
 			bt.position=Vector2(menu_bt_x,menu_bt_y)
 			bt.mouse_entered.connect(_mouse_enter.bind(bt))
 			bt.mouse_exited.connect(_mouse_exit.bind(bt))
-			menu_bt_y+=150
+			menu_bt_y+=65
 
 func _mouse_enter(node:Label):
 	var tween = node.create_tween()
 	tween.tween_property(node,"theme_override_styles/normal:bg_color"
 	,Color(0.573, 0.776, 0.808)
-	,0.2).set_ease(Tween.EASE_IN)
+	,0.1).set_ease(Tween.EASE_IN)
 	#node.get("theme_override_styles/normal").bg_color=Color(0.573, 0.776, 0.808)
 	#node.modulate = Color(0.369, 0.655, 0.702)
 	#Color(0.573, 0.776, 0.808)
@@ -36,3 +38,43 @@ func _mouse_exit(node:Label):
 	tween.tween_property(node,"theme_override_styles/normal:bg_color"
 	,Color(0.369, 0.655, 0.702)
 	,0.2).set_ease(Tween.EASE_OUT)
+	
+func _menu_show():
+	var delay:float=0
+	flag=false
+	for bt in get_children():
+		if bt is Label and bt.name!="menu_bt":
+			var tween = bt.create_tween()
+			tween.set_parallel(true)
+			tween.tween_property(bt,"position:x"
+			,900,0.04).set_delay(delay).set_ease(tween.EASE_IN)
+			tween.tween_property(bt,"modulate:a"
+			,1,0.04).set_delay(delay).set_ease(tween.EASE_IN)
+			bt.mouse_filter=Label.MOUSE_FILTER_STOP
+			delay+=0.04
+
+func _menu_hide():
+	var delay:float=0
+	flag=true
+	for bt in get_children():
+		if bt is Label and bt.name!="menu_bt":
+			var tween = bt.create_tween()
+			tween.set_parallel(true)
+			tween.tween_property(bt,"position:x"
+			,1000,0.04).set_delay(delay).set_ease(tween.EASE_OUT)
+			tween.tween_property(bt,"modulate:a"
+			,0,0.04).set_delay(delay).set_ease(tween.EASE_OUT)
+			bt.mouse_filter=Label.MOUSE_FILTER_IGNORE
+			delay+=0.04
+
+func _menu_input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if flag:
+			_menu_show()
+		else:
+			_menu_hide()
+			
+			
+func _exit_input(event):
+	if (event is InputEventScreenTouch and event.pressed) or (event is InputEventMouseButton and event.pressed):
+		get_tree().quit()
