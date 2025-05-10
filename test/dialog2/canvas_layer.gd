fdacc1c82c736557cd099952fe219a64a1bed89c
prismaticboy <2779435956@qq.com>
2025-05-10 22:54:30 +0800

    shoot

--- test/dialog2/canvas_layer.gd
+++ test/dialog2/canvas_layer.gd
@@ -1,11 +1,19 @@
 extends CanvasLayer
-@onready var sprite: Sprite2D = $"../Sprite"
-@onready var player: Sprite2D = $"../player"
+
 @onready var sub_viewport: SubViewport = $SubViewportContainer/SubViewport
 
-func _ready() -> void:
-	sub_viewport.add_child(sprite.duplicate())
-	sub_viewport.add_child(player.duplicate())
 
+func _ready() -> void:
+	get_shoot(get_tree().current_scene)
+
+func get_shoot(node):
+	for child in node.get_children():
+		print(child.name)
+		if child.name=="view":
+			return
+		if child is Sprite2D:
+			sub_viewport.add_child(child.duplicate())
+		else:
+			get_shoot(child)
 	await RenderingServer.frame_post_draw
 	sub_viewport.get_texture().get_image().get_region(Rect2i(0, 0, 288, 162)).save_png("user://Screenshot1.png")
