69dd4f150592231887a9963b01921801403d6b2d
prismaticboy <2779435956@qq.com>
2025-05-19 21:57:04 +0800

    save bt status

--- test/dialog2/save_bt.gd
+++ test/dialog2/save_bt.gd
@@ -6,12 +6,8 @@ func _ready():
 	
 func _on_gui_input(event,node:Panel):
 	if event is InputEventMouseButton and event.pressed and event.button_index==MOUSE_BUTTON_LEFT:
-		$SubViewportContainer.get_shoot(get_tree().current_scene)
-		await RenderingServer.frame_post_draw
-		$SubViewportContainer/SubViewport.get_texture().get_image().get_region(Rect2i(0, 0, 144, 81)).save_png("user://"+self.name+".png")
-		var time = Time.get_datetime_dict_from_system()
-		$time.text = "时间:"+str(time.year)+"-"+str(time.month)+"-"+str(time.day)+" "+str(time.hour)+":"+str(time.minute)+":"+str(time.second)
-		get_content()
+		update_bt_status()
+		save_bt_status()
 		
 func get_content():
 	var content=get_tree().current_scene.get_node("Panel/content")
@@ -21,7 +17,21 @@ func get_content():
 		$dialog_data.max_lines_visible=2
 		$dialog_data.clip_text=true
 		$dialog_data.text=content.text
-
-func save_bt_status():
+		
+func update_bt_status():
+	$SubViewportContainer.get_shoot(get_tree().current_scene)
+	var time = Time.get_datetime_dict_from_system()
+	$time.text = "时间:"+str(time.year)+"-"+str(time.month)+"-"+str(time.day)+" "+str(time.hour)+":"+str(time.minute)+":"+str(time.second)
+	get_content()	
 	
+func save_bt_status():
+	var config=ConfigFile.new()
+	config.set_value("bt", "number", $number.text)
+	config.set_value("bt", "dialog_data", $dialog_data.text)
+	config.set_value("bt", "time", $time.text)
+	await RenderingServer.frame_post_draw
+	$SubViewportContainer/SubViewport.get_texture().get_image().get_region(Rect2i(0, 0, 144, 81)).save_png("user://"+self.name+".png")
+	config.save("user://bt"+name+".cfg")
+
+func load_bt_status():
 	pass
